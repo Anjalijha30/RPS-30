8e6034f7891b48b8b4e29b686dfd73d437a1bb64
Anjali Jha <ganjalijha2000@gmail.com>
2020-09-01 20:18:50 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,1 +1,80 @@
+import random
 
+# Print multiline instructions
+# Performing concatenation of string
+print("Winning Rules of Rock paper scissors game as follows: \n"+"Rock vs paper -> paper wins \n"+"Rock vs scissors -> Rock wins \n"+"Paper vs scissors ->scissors wins \n")
+while True:
+    print("Enter choice \n 1. Rock \n 2. Paper \n 3.Scissors \n")
+
+    # take the input from user
+    choice =int(input("User turn: "))
+
+    # OR is the short-circuit operator
+    # if any one of the condition is true
+    # then it return True value
+
+    # looping until user enter invalid input
+    while choice >3 or choice<1:
+        choice=int(input("enter valid input: "))
+    # initialize value of choice _name variable
+    # corresponding to the choice value
+    if choice ==1:
+        choice_name= 'Rock'
+    elif choice ==2:
+        choice_name= 'paper'
+    else:
+        choice_name= 'scissor'
+
+    # print user choice
+    print("user choice is: "+choice_name)
+    print("\nNow its computer turn..........")
+
+    # computer chooses randomly any number
+    # among 1, 2, 3.Using randint method
+    # of module
+    comp_choice=random.randint(1,3)
+
+    # looping until comp_choice value
+    # is equal ti the choice value
+    while comp_choice==choice:
+        comp_choice=random.randint(1,3)
+
+    # initialize value of comp_choice_name
+    # variable corresponding to the choice value
+    if comp_choice==1:
+        comp_choice_name='Rock'
+    elif comp_choice==2:
+        comp_choice_name='paper'
+    else:
+        comp_choice_name='scissors'
+    print("Computer choice is: "+comp_choice_name)
+    print(choice_name+" Vs "+comp_choice_name)
+
+    # condition for winning
+    if((choice==1 and comp_choice==2) or (choice==2 and comp_choice==1)):
+        print("paper wins =>",end="")
+        result="paper"
+    elif((choice==1 and comp_choice==3)or (choice==3 and comp_choice==1)):
+        print("Rock wins =>",end="")
+        result="Rock"
+    else:
+        print("Scissor wins =>",end="")
+        result="Scissor"
+
+    # printing either user or computer wins
+    if result == choice_name:
+        print("  <== User wins ==>")
+    else:
+        print("  <== computer wins ==>")
+
+    print("Do you want to play again? (Y/N)")
+    ans=input()
+
+    # if the user input is n ot N then condition is True
+    if ans=='n' or  ans=='N':
+        break
+
+# after coming out of the while loop
+# we print thanks for playing
+
+print("\n Thanks for playing ")
